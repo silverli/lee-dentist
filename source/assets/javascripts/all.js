7e6896349548c96d668175f08d3dd47a71d44bc1
Lindsay Silver <lindsay@Lindsays-Air.home>
2016-02-22 02:03:46 -0500

    Adding tabs to about page, making footer a bit better, making font in logo an svg vector path, creating new constrained container and constrained-small container

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -2,3 +2,12 @@
 //= require jrespond/jRespond
 //= require responders
 //= require navigation
+
+$(document).ready(function() {
+    $('.tabs a').on('click', function() {
+      if (!$(this).hasClass('active')) {
+        $('.tabs a').toggleClass('active');
+        $('.tab-content div').toggleClass('active');
+      }
+    });
+});
